8a23be00d3053d4db4f09a9fe2c5c74f3f3ad52e
ahakanen <ahakanen@student.hive.fi>
2021-09-20 20:05:40 +0300

    first commit

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+	public GameController gameController;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("Collision with " + other);
+		Debug.Log("DEATH!");
+		gameController.Death();
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject football;
+	public float forceMultiplier = 500;
+	public TextMeshProUGUI scoreText;
+	public TextMeshProUGUI gameOverText;
+	public AudioSource hitSound;
+	bool isDead = false;
+	int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+		InitGame();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+		{
+			if (isDead == true)
+			{
+				InitGame();
+				return;
+			}
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector3 distance = mousePos - football.transform.position;
+			if (distance.magnitude < 15)
+			{
+				Vector3 force = distance.magnitude * distance.normalized * forceMultiplier;
+				football.GetComponent<Rigidbody2D>().AddForce(-force);
+				score++;
+				scoreText.text = "Score: " + score.ToString();
+				hitSound.Play();
+			}
+		}
+    }
+
+	public void Death()
+	{
+		isDead = true;
+		scoreText.text = "Score: " + score.ToString();
+		gameOverText.gameObject.SetActive(true);
+		//show score and show "touch to restart"
+	}
+
+	public void InitGame()
+	{
+		score = 0;
+		scoreText.text = "Score: " + score.ToString();
+		football.transform.position = Vector3.zero;
+		isDead = false;
+		gameOverText.gameObject.SetActive(false);
+	}
+}
